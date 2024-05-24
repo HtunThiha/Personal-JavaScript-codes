7fb1568bfef75a7717c3382ee7b8794e1a5c0d89
HtunThiha <htunthiha.dev@gmail.com>
2024-05-24 21:29:49 +0800

    Update compound-interest-calculator.js

--- compound-interest-calculator.js
+++ compound-interest-calculator.js
@@ -1,1 +1,8 @@
+const input = require('readline-sync');
+
+console.log("This program will calculate compound interest");
+
+let baseAmount = input.question("Enter the base amount: ");
+let totalTime = input.question("How many years will the interest commence?: ");
+let interval = input.question("In how much period of time the interest will be increased once?: ");
 
